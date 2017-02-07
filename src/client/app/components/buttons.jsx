5cdfa694e6ba53a935de3861afc4e8dfe08d59e4
datomo <dave.lengw@gmail.com>
2017-02-07 03:23:24 +0100

    small button

--- src/client/app/components/buttons.jsx
+++ src/client/app/components/buttons.jsx
@@ -5,7 +5,7 @@ export default class Button extends React.Component {
 	render() {
 		return (
 			<div className="row justify-content-md-center">
-				<button type="button" onClick={this.props.onClick} className="btn btn-danger btn-lg ">
+				<button type="button" onClick={this.props.onClick} className="btn btn-outline-danger btn-lg ">
 					{this.props.children}
 				</button>
 			</div>	
