252356e16b8e262958e4f22f6499e86e7c919aa8
datomo <dave.lengw@gmail.com>
2017-02-11 16:48:09 +0100

    started with the cardstack

--- src/client/app/components/cardStack.jsx
+++ src/client/app/components/cardStack.jsx
@@ -5,7 +5,11 @@ import Card from './cards.jsx'
 export default class CardStack extends React.Component {
 	render() {
 		return (
+			<div className="card-stack">
 				<Card id="card01"/>
+				<Card id="card02"/>
+				<Card id="card03"/>
+			</div>	
 		);
 	}
 };
